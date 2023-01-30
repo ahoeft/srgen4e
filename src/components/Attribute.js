f87e4d4d39a0637d537c973d04a3c02817471d0c
Andy Hoeft <andyh@guardiansoft.com>
2023-01-30 13:06:41 -0600

    add build points and start passing state around'

--- src/components/Attribute.js
+++ src/components/Attribute.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 class Attribute extends React.Component {
@@ -14,21 +14,27 @@ class Attribute extends React.Component {
 
     handleAdd() {
         var current = this.state.value;
+        var bpCost = 0;
         var newVal = current;
         if((current + 1) <= 6) {
             newVal = current + 1;
+            bpCost = -10;
         }
         this.setState({value: newVal});
+        this.props.updateAttribute(bpCost);
     }
 
     handleSubtract() {
+        var bpCost = 0;
         var current = this.state.value;
         var newVal = 0;
         if((current - 1) >= 0) {
             newVal = current - 1;
+            bpCost = 10;
         }
 
         this.setState( {value: newVal});
+        this.props.updateAttribute(bpCost);
     }
 
     render() {
--- src/components/Attributes.js
+++ src/components/Attributes.js
@@ -1,38 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Attribute from './Attribute';
 import Card from 'react-bootstrap/Card';
 
 class Attributes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            buildPoints: 400
+        };
+        
+    }
+    updateAttribute = (bpCost) => {
+        var newBP = this.state.buildPoints + bpCost
+
+        this.setState({buildPoints: newBP})
+    }
+
+    
     render() {
         return <Card style={{ width: '18rem', textAlign: 'left'}}>
             <Card.Title>Attributes</Card.Title>
             <Card.Body>
+                <div>{this.state.buildPoints}</div>
                 <Card style={{margin: '1rem'}}>
                     <Card.Title>Physical</Card.Title>
                     <Card.Body>
-                        <Attribute name="Agility"/>
-                        <Attribute name="Body"/>
-                        <Attribute name="Reaction"/>
-                        <Attribute name="Strength"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Agility"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Body"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Reaction"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Strength"/>
                     </Card.Body>
                 </Card>             
                 <Card style={{margin: '1rem'}}>
                     <Card.Title>Mental</Card.Title>
                     <Card.Body>
-                        <Attribute name="Charisma"/>
-                        <Attribute name="Intuition"/>
-                        <Attribute name="Logic"/>
-                        <Attribute name="Willpower"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Charisma"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Intuition"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Logic"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Willpower"/>
                     </Card.Body>
                 </Card>
                 <Card style={{margin: '1rem'}}>
                     <Card.Title>Special</Card.Title>
                     <Card.Body>
-                        <Attribute name="Edge"/>
-                        <Attribute name="Essence"/>
-                        <Attribute name="Initiative"/>
-                        <Attribute name="Magic"/>
-                        <Attribute name="Resonance"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Edge"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Essence"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Initiative"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Magic"/>
+                        <Attribute updateAttribute={this.updateAttribute} name="Resonance"/>
                     </Card.Body>
                 </Card>
             </Card.Body>
